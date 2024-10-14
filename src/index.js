7667a40a054215b1f584da4db5b32e0c5646e814
Phelix <phelixmbani@gmail.com>
2024-10-14 10:11:02 +0300

    phase 1 code challange

--- src/index.js
+++ src/index.js
@@ -2,23 +2,79 @@
 
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  // Handle the click event for each ramen
+  const imageDetail = document.getElementById('ramen-detail');
+  document.getElementById('comment-display').textContent = ramen.comment
+  document.getElementById('rating-display').textContent = ramen.rating
+  imageDetail.querySelector('.detail-image').src = ramen.image;
+  imageDetail.querySelector('.name').textContent = ramen.name;
+  imageDetail.querySelector('.restaurant').textContent = ramen.restaurant;
+  
 };
 
 const addSubmitListener = () => {
-  // Add code
-}
+  const newRamenForm = document.getElementById('new-ramen');
+  newRamenForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+
+  
+    const name = document.getElementById('new-name').value;
+    const restaurant = document.getElementById('new-restaurant').value;
+    const image = document.getElementById('new-image').value;
+    const rating = document.getElementById('new-rating').value;
+    const comment = document.getElementById('new-comment').value;
+
+    
+    const newRamen = {
+      name,
+      restaurant,
+      image,
+      rating,
+      comment,
+    };
+
+  
+    fetch('http://localhost:3000/ramens', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newRamen),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        
+        displayRamens();
+      })
+  });
+};
 
 const displayRamens = () => {
-  // Add code
+  const ramenMenu = document.getElementById('ramen-menu');
+
+
+  fetch('http://localhost:3000/ramens')
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((ramen) => {
+        const ramenImage = document.createElement('img');
+        
+        ramenImage.src = ramen.image; 
+
+        
+        ramenImage.addEventListener('click', () => handleClick(ramen));
+
+        ramenMenu.appendChild(ramenImage);
+      });
+    })
 };
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
+  displayRamens();
+  addSubmitListener();
+};
 
-main()
+main();
 
 // Export functions for testing
 export {
